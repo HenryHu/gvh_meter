212d9af6ff002e9cde1683c813b578d4ec0a9e8c
Henry Hu <henry.hu.sh@gmail.com>
2021-07-21 00:44:21 -0400

    make arduino-builder work

---
+++ src/main.ino
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+#include <TFT_eSPI.h>
+
+#include <PubSubClient.h>
+
+// #include <Wire.h>
+#include <WiFi.h>
+// #include <FS.h>
+#include <BLEScan.h>
+#include <BLEUtils.h>
+#include <BLEDevice.h>
+
+#include "lvgl.h"
+
+
