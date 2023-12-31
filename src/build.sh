4c25bc5a72a921f1334eb55ea18687cc8fc3c9ed
Henry Hu <henry.hu.sh@gmail.com>
2023-12-31 12:41:11 -0500

    update build scripts

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 mkdir -p /tmp/arduino_out
-arduino-builder -prefs build.partitions=huge_app -verbose -libraries $HOME/proj/arduino/libraries -hardware $HOME/proj/arduino/hardware -tools /usr/local/arduino/tools -tools /usr/bin -fqbn esp32:esp32:esp32 -build-path /tmp/arduino_out -build-cache $HOME/proj/arduino/cache main.ino
+
+ARGS="-prefs build.partitions=huge_app -prefs upload.maximum_size=3145728 -verbose -libraries $HOME/proj/arduino/libraries -hardware $HOME/proj/arduino/hardware -tools /usr/local/arduino/tools -tools /usr/bin -fqbn esp32:esp32:esp32 -build-path /tmp/gvh_meter -build-cache $HOME/proj/arduino/cache main.ino"
+
+arduino-builder -dump-prefs $ARGS
+arduino-builder $ARGS
--- src/flash.sh
+++ src/flash.sh
@@ -1,1 +1,7 @@
-esptool.py --chip esp32 --port /dev/cuaU0 --baud 921600 write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x10000 /tmp/arduino_out/main.ino.bin
+#!/bin/sh
+
+# Full command:
+# esptool.py --chip esp32 --port "{serial.port}" --baud {upload.speed}  --before default_reset --after hard_reset write_flash -z --flash_mode {build.flash_mode} --flash_freq {build.flash_freq} --flash_size detect 0xe000 "{runtime.platform.path}/tools/partitions/boot_app0.bin" 0x1000 "{runtime.platform.path}/tools/sdk/bin/bootloader_{build.boot}_{build.flash_freq}.bin" 0x10000 "{build.path}/{build.project_name}.bin" 0x8000 "{build.path}/{build.project_name}.partitions.bin"
+
+#esptool.py write_flash 0x8000 0xc00 /tmp/arduino_out/main.ino.partitions.bin
+esptool.py --chip esp32 --port /dev/cuaU1 --baud 921600 write_flash -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x10000 /tmp/gvh_meter/main.ino.bin
